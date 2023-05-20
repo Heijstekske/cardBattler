5af42fa6be6b210e4b214d750e36ea104ac66756
Heijstekske <heijstekker@gmail.com>
2023-05-20 13:26:54 +0200

    initial commit

---
+++ src/main/java/com/example/cardbattler/CardBattlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardbattler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardBattlerApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(CardBattlerApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/example/cardbattler/CardBattlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardbattler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardBattlerApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
